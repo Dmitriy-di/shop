49d75a78e091b2faaa88168dff11b6c494684a89
Gelato <you@example.com>
2023-02-22 19:24:22 +0300

    Project created

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<div id="app">
+  <div>Hello my friend</div>
+</div>
 </template>
 
 <style lang="scss">
